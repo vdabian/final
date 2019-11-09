24cdc87a09f942e2158a16b796696d19856e3468
Vinícius Dabian <vinicius_dabian@hotmail.com>
2019-11-09 20:33:15 -0300

    Created Class SavingsAccountPlus

---
+++ src/entities/SavingsAccountPlus.java
@@ -0,0 +1,15 @@
+package entities;
+
+
+public class SavingsAccountPlus extends SavingsAccount {
+//public class SavingsAccountPlus extends SavingsAccount {
+//impossible when SavingsAccount is a final class
+
+	//@Override
+	//public void withdraw(double amount) {
+	//	balance -= amount + 2.00;
+	//}
+	//impossible when withdraw is a final method
+	
+	
+}
